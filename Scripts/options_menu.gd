4de08eade3029344f7eb695e2df4afccbad30b60
Wpan0n <airsplat101@gmail.com>
2024-05-17 15:41:29 -0400

    Added Fullscreen,Borderless,Windowed, functionality

--- Scripts/options_menu.gd
+++ Scripts/options_menu.gd
@@ -55,3 +55,29 @@ func _on_preview_button_pressed() -> void:
 	# Play the music preview.
 	level_music_player.stream = level_music
 	level_music_player.play()
+
+
+func _on_fullscreen_toggled(button_pressed):
+	if button_pressed == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+func _on_borderless_toggled(button_pressed):
+	if button_pressed == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+func _on_v_sync_toggled(button_pressed):
+	if button_pressed == true:
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
+	else:
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+
+
+func _on_windowed_toggled(toggled_on):
+	if toggled_on == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
