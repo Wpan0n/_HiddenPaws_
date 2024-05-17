a045d3ca93f7381c8cadc1b1608e81e9fce460f4
Wpan0n <airsplat101@gmail.com>
2024-05-17 16:13:29 -0400

    nothing

--- Scripts/options_menu.gd
+++ Scripts/options_menu.gd
@@ -80,4 +80,4 @@ func _on_windowed_toggled(toggled_on):
 	if toggled_on == true:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 	else:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)	
