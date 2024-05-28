10fcc87d306809e4f319466fa960ba6a3e8ccd1c
Wpan0n <airsplat101@gmail.com>
2024-05-27 20:58:08 -0400

    Sprite Click Sfx

--- Scripts/testsprite.gd
+++ Scripts/testsprite.gd
@@ -1,8 +1,14 @@
 extends Sprite2D
 
+@onready var audio_player = $"../AudioStreamPlayer"  # Use the correct node path
+
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			if is_pixel_opaque(get_local_mouse_position()):
 				print("sprite clicked")
-				modulate = Color(0, 24, 201)  # Change the sprite color to red
+				modulate = Color(1, 0, 0)  # Change the sprite color to red
+				if audio_player:  # Check if the audio_player node is valid
+					audio_player.play()  # Play the sound effect
+				else:
+					print("AudioStreamPlayer node not found")
