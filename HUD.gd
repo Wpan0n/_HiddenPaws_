eba33b90c814e4360561ba79bb50dca91502d22f
Lelouch0v0 <140763092+Wpan0n@users.noreply.github.com>
2024-05-28 22:08:00 -0400

    touch up

--- HUD.gd
+++ HUD.gd
@@ -3,8 +3,7 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	$catcount.text = "Cats: 0"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
