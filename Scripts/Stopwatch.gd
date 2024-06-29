6e928f2482ec6a3700a9ccd61730b768504ff8bb
Wpan0n <airsplat101@gmail.com>
2024-06-29 17:32:39 -0400

    stop watch update and something else idk

--- Scripts/Stopwatch.gd
+++ Scripts/Stopwatch.gd
@@ -9,11 +9,11 @@ func _process(delta: float) -> void:
 
 # Function to update the displayed time on the label
 func update_timer_display() -> void:
-	var hours = int(time_elapsed / 3600)  # Calculate the number of hours by dividing the total seconds by 3600 (number of seconds in an hour)
-	var minutes = int(time_elapsed) % 3600 / 60  # Calculate the number of minutes by first taking the integer part of time_elapsed, then using modulo 3600 to get the remaining seconds after hours, and dividing by 60
+	var hours = int(time_elapsed / 3600.0)  # Calculate the number of hours by dividing the total seconds by 3600.0 (number of seconds in an hour)
+	var minutes = int(time_elapsed / 60.0) % 60  # Calculate the number of minutes by dividing the total seconds by 60.0, then taking modulo 60
 	var seconds = int(time_elapsed) % 60  # Calculate the number of seconds by taking the remainder of total seconds divided by 60
-	var milliseconds = int((time_elapsed - int(time_elapsed)) * 100)  # Calculate the number of milliseconds by taking the fractional part of time_elapsed and multiplying by 100
+	var milliseconds = int((time_elapsed - int(time_elapsed)) * 100.0)  # Calculate the number of milliseconds by taking the fractional part of time_elapsed and multiplying by 100.0
 
 	# Format the time string as "hh:mm:ss:ms" (hours:minutes:seconds:milliseconds)
-	var time_string = String("%02d:%02d:%02d:%02d" % [hours, minutes, seconds, milliseconds])
+	var time_string = "%02d:%02d:%02d:%02d" % [hours, minutes, seconds, milliseconds]
 	text = time_string  # Set the text of the label to the formatted time string
