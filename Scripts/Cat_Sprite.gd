3d63bb561187b6b378b50699e9df4f9c52e86166
Lelouch0v0 <140763092+Wpan0n@users.noreply.github.com>
2025-04-09 09:18:16 -0400

    Update Cat_Sprite.gd
    fixed the gray cat

--- Scripts/Cat_Sprite.gd
+++ Scripts/Cat_Sprite.gd
@@ -3,21 +3,21 @@ extends Sprite2D
 signal sprite_color_changed
 
 @onready var audio_player = $"../AudioStreamPlayer"
-var clicked = false  # Variable to track if the sprite has been clicked
+var clicked = false  # Track click state
 
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			if is_pixel_opaque(get_local_mouse_position()):
-				print("sprite clicked")  # Print message to the console
-				modulate = Color(0.5, 0.5, 0.5)  # Changed to grey (RGB 0.5, 0.5, 0.5)
-				if audio_player:
-					audio_player.play()  # Play the sound effect
-				else:
-					print("AudioStreamPlayer node not found")  # Print error message
+				print("sprite clicked")
+				modulate = Color("#bebebe")  # Changed to hex color code
 				
-				# Emit a signal to notify that the sprite's color has changed only if not clicked before
+				if audio_player:
+					audio_player.play()
+				else:
+					print("AudioStreamPlayer node not found")
+				
 				if not clicked:
 					emit_signal("sprite_color_changed")
-					print("Signal emitted")  # Print a message when the signal is emitted
-					clicked = true  # Set the clicked state to true to prevent further score increments
+					print("Signal emitted")
+					clicked = true
